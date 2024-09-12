1a7f7acbfa275f2558499149399216ad73c1386d
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-09-12 21:33:05 +0300

    some dependecies added

---
+++ customerservice/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // React dosyaları için
+    "./public/index.html", // HTML dosyaları için (isteğe bağlı)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
